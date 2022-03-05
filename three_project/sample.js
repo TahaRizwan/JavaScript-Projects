492c8054a4f892894503e787b7b29d88617401d6
TahaRizwan_3 <taharizwanmughal@gmail.com>
2022-03-05 14:48:16 +0500

    CSS JS ADDED

--- three_project/sample.js
+++ three_project/sample.js
@@ -1,76 +1,67 @@
 const reviews = [
-    {
-      id: 1,
-      name: "susan smith",
-      job: "web developer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-      text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {
-      id: 2,
-      name: "anna johnson",
-      job: "web designer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-      text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      text:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-  ];
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
 
-  let person_name = document.getElementById('person-name') 
-  let person_job = document.getElementById('person-job') 
-  let person_text = document.getElementById('person-text') 
-  let prev = document.getElementById('prev') 
-  let next = document.getElementById('next') 
-  let surprise = document.getElementById('surprise')
-  let i=0
-  prev.addEventListener('click',prevData)
-  function prevData(){
-    i--;
-    person_name.textContent = reviews[i].name
-    person_job.textContent = reviews[i].job
-    document.getElementById('person-img').src = reviews[i].img 
-    person_text.textContent = reviews[i].text
-    if(i==0){
-      i++
-    }
+let person_name = document.getElementById("person-name");
+let person_job = document.getElementById("person-job");
+let person_text = document.getElementById("person-text");
+let person_img = document.getElementById("person-img");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let surprise = document.getElementById("surprise");
+let currentItem = 0;
+function showPerson(currentItem) {
+  person_name.textContent = reviews[currentItem].name;
+  person_job.textContent = reviews[currentItem].job;
+  person_img.src = reviews[currentItem].img;
+  person_text.textContent = reviews[currentItem].text;
+}
+
+prev.addEventListener("click", prevData);
+function prevData() {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
   }
-  next.addEventListener('click',nextData)
-  function nextData(){
-    i++;
-    person_name.textContent = reviews[i].name
-    person_job.textContent = reviews[i].job
-    document.getElementById('person-img').src = reviews[i].img 
-    person_text.textContent = reviews[i].text
-    if(i==3){
-      i--;
-    }
+  showPerson(currentItem);
+}
+next.addEventListener("click", nextData);
+function nextData() {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
   }
-  surprise.addEventListener('click',surpriseData)
-  function surpriseData(){
-    i=Math.floor(Math.random()*4);
-    person_name.textContent = reviews[i].name
-    person_job.textContent = reviews[i].job
-    document.getElementById('person-img').src = reviews[i].img 
-    person_text.textContent = reviews[i].text
-  }
+  showPerson(currentItem);
+}
+surprise.addEventListener("click", surpriseData);
+function surpriseData() {
+  currentItem = Math.floor(Math.random() * 4);
+  showPerson(currentItem);
+}
